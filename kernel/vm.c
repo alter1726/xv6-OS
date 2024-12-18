7dc4fd16f8c4b93c6dd1a3c7d1d90b84a49479d0
alter1726 <3209542609@qq.com>
2024-12-18 21:47:12 +0800

    finished work1 of lab3

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+//递归打印页表
+int
+ycz_pgtblprint(pagetable_t pagetable,int depth)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V))  //页表项有效
+    {
+      printf("..");
+      for (int j = 0; j < depth; j++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n",i,pte,PTE2PA(pte));
+
+      //是否不在最后一层
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        uint64 child = PTE2PA(pte);
+        ycz_pgtblprint((pagetable_t)child,depth+1);
+      }
+    }
+  }
+  return 0;
+}
+
+int
+ycz_vmprint(pagetable_t pagetable){
+  printf("page table %p\n",pagetable);
+  return ycz_pgtblprint(pagetable,0);
+}
